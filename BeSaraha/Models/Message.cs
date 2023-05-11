41dd89185f622b9bc9934dcc0eb5d7cc1a48b03e
Raid7 Raid7 <thmrgb8@gmail.com>
2023-05-11 06:49:32 +0300

    added models

---
+++ BeSaraha/Models/Message.cs
@@ -0,0 +1,9 @@
+﻿namespace BeSaraha.Models
+{
+    public class Message
+    {
+        public int UserId { get; set; }
+        public string Text { get; set; }
+        public DateTime date { get; set; }
+    }
+}
