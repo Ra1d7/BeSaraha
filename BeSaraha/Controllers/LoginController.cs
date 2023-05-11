005010bc47a0f1128a01246bc7742cd8977dfed8
Raid7 Raid7 <thmrgb8@gmail.com>
2023-05-11 07:05:05 +0300

    implemented getting register data from front-end

--- BeSaraha/Controllers/LoginController.cs
+++ BeSaraha/Controllers/LoginController.cs
@@ -4,6 +4,8 @@ namespace BeSaraha.Controllers
 {
     public class LoginController : Controller
     {
+        public record RegisterDTO(string email,string firstname,string lastname,string password,bool keeplogin=false);
+        public record LoginDTO(string email,string password,bool keeplogin=false);
         public IActionResult Login()
         {
             return View();
@@ -11,6 +13,20 @@ namespace BeSaraha.Controllers
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterDTO register) 
+        {
+            //do stuff
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginDTO login)
+        {
+            // login
+            throw new NotImplementedException();
         }
     }
 }
