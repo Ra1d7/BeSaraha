403a86d143f6e982933079cacd35984e48bdef7b
Raid7 Raid7 <thmrgb8@gmail.com>
2023-05-14 02:03:20 +0300

    adding changing profile picture ability (still not working)

---
+++ BeSaraha/Controllers/UserSettings.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BeSaraha.Controllers
+{
+    public class UserSettings : Controller
+    {
+        public IActionResult Settings()
+        {
+            return View();
+        }
+    }
+}
