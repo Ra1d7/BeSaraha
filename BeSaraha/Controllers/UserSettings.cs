11ead5f2ef2d2adcbc0a1ec7506f9183d7a63187
Raid7 Raid7 <thmrgb8@gmail.com>
2023-05-14 07:30:08 +0300

    saved images to a folder

--- BeSaraha/Controllers/UserSettings.cs
+++ BeSaraha/Controllers/UserSettings.cs
@@ -1,18 +1,24 @@
 ﻿using BeSaraha.DataAccess;
 using BeSaraha.Models;
 using Dapper;
+using Firebase.Storage;
 using Microsoft.AspNetCore.Mvc;
+using NuGet.Packaging;
 
 namespace BeSaraha.Controllers
 {
     public class UserSettings : Controller
     {
+        private string[] images = { ".jpg", ".png", ".jpeg", ".webp" };
         private readonly BeSarahaDB _db;
+        private readonly IWebHostEnvironment _env;
+
         public record UserSettingsDTO(string firstname,string lastname,string profileurl,IFormFile? picture = null);
 
-        public UserSettings(BeSarahaDB db)
+        public UserSettings(BeSarahaDB db,IWebHostEnvironment env)
         {
             _db = db;
+            _env = env;
         }
         public async Task<IActionResult> Settings()
         {
@@ -26,9 +32,22 @@ namespace BeSaraha.Controllers
         [HttpPost]
         public async Task<IActionResult> SaveSettings(UserSettingsDTO user)
         {
-            if(ModelState.IsValid)
+             string filename = user.picture.FileName;
+            if (ModelState.IsValid && images.Contains(Path.GetExtension(filename)))
             {
-                TempData["success"] = "Your profile has been updated!";
+                var stream = user.picture.OpenReadStream();
+                string filePath = Path.Combine(_env.WebRootPath, "assets", filename);
+                using (Stream fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    await user.picture.CopyToAsync(fileStream);
+                    using var connection = _db.GetConnection();
+                    string userid = HttpContext.User.Claims.ToArray()[2].Value;
+                    int rows = await connection.ExecuteAsync("UPDATE USERS SET picture = @filename WHERE id = @userid", new { filename, userid });
+                    if (rows == 1)
+                    {
+                        TempData["success"] = "Your profile has been updated!";
+                    }
+                }
             }
             else
             {
