953dce38e26cba814fdc0a701cfbd8f17f8ad3fd
Raid7 Raid7 <thmrgb8@gmail.com>
2023-05-14 03:27:27 +0300

    added usersettings and made it recieve new fields for the user

--- BeSaraha/Controllers/UserSettings.cs
+++ BeSaraha/Controllers/UserSettings.cs
@@ -1,12 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BeSaraha.DataAccess;
+using BeSaraha.Models;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BeSaraha.Controllers
 {
     public class UserSettings : Controller
     {
-        public IActionResult Settings()
+        private readonly BeSarahaDB _db;
+        public record UserSettingsDTO(string firstname,string lastname,string profileurl,IFormFile? picture = null);
+
+        public UserSettings(BeSarahaDB db)
         {
-            return View();
+            _db = db;
+        }
+        public async Task<IActionResult> Settings()
+        {
+            string userid = HttpContext.User.Claims.ToArray()[2].Value;
+            using var connection = _db.GetConnection();
+            var user = await connection.QueryFirstOrDefaultAsync<User>("SELECT * FROM Users WHERE id = @userid", new { userid });
+            var userDto = new UserSettingsDTO(user.Firstname,user.Lastname,user.ProfileUrl);
+            return View(userDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveSettings(UserSettingsDTO user)
+        {
+            if(ModelState.IsValid)
+            {
+                TempData["success"] = "Your profile has been updated!";
+            }
+            else
+            {
+                TempData["error"] = "An error has occured while saving changes";
+            }
+            return RedirectToAction("Settings", "UserSettings");
         }
     }
 }
--- BeSaraha/Models/User.cs
+++ BeSaraha/Models/User.cs
@@ -1,4 +1,6 @@
-﻿namespace BeSaraha.Models
+﻿using System.ComponentModel;
+
+namespace BeSaraha.Models
 {
     public class User
     {
@@ -7,6 +9,7 @@
         public string Password { get; set; }
         public string Firstname { get; set; }
         public string Lastname { get; set; }
+        [DisplayName("Profile Picture")]
         public string Picture { get; set; }
         public string ProfileUrl { get; set; } = Random.Shared.Next(10000, 100000).ToString();
         public int followersCount { get; set; } = 0;
